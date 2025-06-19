34b36e43e7e6a1951c8ce31277cef2a1cf06b792
bipan7729922595 <vipinhcl@gmail.com>
2025-06-19 01:31:08 +0100

    Update pet.service.ts

--- PetsAlone.Angular/ClientApp/src/app/create-pet/pet.service.ts
+++ PetsAlone.Angular/ClientApp/src/app/create-pet/pet.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PetService {
@@ -7,10 +8,18 @@ export class PetService {
 
   getAllPets(petType?: number) {
     const params = petType != null ? { petType: petType.toString() } : {};
-    return this.http.get<any>('api/pets/all', { params });
+    return this.http.get<any>('http://localhost:27544/api/pets/all', { params });
   }
 
   createPet(pet: any) {
-    return this.http.post('api/pets/create', pet);
+    const token = localStorage.getItem('currentUser'); 
+    
+    /*const headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}` 
+    });*/
+    console.log('Token:', token);//console.log('Header:',headers)
+    /*return this.http.post('http://localhost:27544/api/pets/create', pet, { headers });*/
+    return this.http.post('http://localhost:27544/api/pets/create', pet, );
   }
 }
